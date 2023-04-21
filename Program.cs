2e45de2bab4e80cfa26ffcf6047afea6fa9e455b
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2023-04-22 00:39:31 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+using static System.Linq.Enumerable;
+
+internal class Program
+{
+    static void Main()
+    {
+        int N = int.Parse( ReadLine() );
+        string[] rl = ReadLine().Split( ' ' );
+
+        foreach( var i in Range( 1, N - 1 ) ) {
+            bool f = false;
+
+            foreach( var j in Range( 0, i ) ) {
+                if( System.Array.IndexOf( rl, rl[i] ) < i ) {
+                    f = true;
+                    WriteLine( "Yes" );
+                    break;
+                }
+            }
+            if( f != true ) {
+                WriteLine( "No" );
+            }
+        }
+        return;
+    }
+}
